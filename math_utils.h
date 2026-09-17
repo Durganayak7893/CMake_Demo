2ffbf4da1a3034a9f9e6295584e3f4960185147e
Durganayak7893 <durgaprasadnaik14@gmail.com>
2026-09-17 12:01:35 +0000

    Fixed windows shared library export

--- math_utils.h
+++ math_utils.h
@@ -1,6 +1,16 @@
-#ifndef MATH_H
-#define MATH_H
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
 
-int add(int a, int b);
+#ifdef _WIN32
+    #ifdef MATHLIB_EXPORTS
+        #define MATHLIB_API __declspec(dllexport)
+    #else
+        #define MATHLIB_API __declspec(dllimport)
+    #endif
+#else
+    #define MATHLIB_API
+#endif
+
+MATHLIB_API int add(int a, int b);
 
 #endif
